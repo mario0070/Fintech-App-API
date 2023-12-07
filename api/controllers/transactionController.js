eb88ce3fc24699f5622fadf5daa80b865570897e
muhammadjamiu abdulganiyu <ganiujamiu03@gmail.com>
2023-12-07 13:18:59 +0100

    Account balalnce

--- api/controllers/transactionController.js
+++ api/controllers/transactionController.js
@@ -23,43 +23,36 @@ const sendTransact = (req, res) => {
     .then(transact_recepient => {
         userSchema.findById(req.body.sender)
         .then(sender => {
-            if(req.body.amount < sender.balance){
-                if(transact_recepient){
-                    const tx = new transactionSchema({
-                        amount : req.body.amount,
-                        recipient : req.body.recipient,
-                        sender : req.body.sender,
-                    })
-                    tx.save()
-                    .then(data => {
-                        const add_ops = parseInt(transact_recepient.balance) + parseInt(req.body.amount)
-                        userSchema.findByIdAndUpdate(req.body.recipient, {"balance" : add_ops  })
+            if(transact_recepient){
+                const tx = new transactionSchema({
+                    amount : req.body.amount,
+                    recipient : req.body.recipient,
+                    sender : req.body.sender,
+                })
+                tx.save()
+                .then(data => {
+                    const add_ops = parseInt(transact_recepient.balance) + parseInt(req.body.amount)
+                    userSchema.findByIdAndUpdate(req.body.recipient, {"balance" : add_ops  })
+                    .then(updated => {
+                        const deduct_ops = parseInt(sender.balance) - parseInt(req.body.amount)
+                        userSchema.findByIdAndUpdate(req.body.sender, {"balance" : deduct_ops  })
                         .then(updated => {
-                            const deduct_ops = parseInt(sender.balance) - parseInt(req.body.amount)
-                            userSchema.findByIdAndUpdate(req.body.sender, {"balance" : deduct_ops  })
-                            .then(updated => {
-                                res.status(200).json({message : "transaction was successful"})
-                            })
+                            res.status(200).json({message : "transaction was successful"})
                         })
-                        .catch(err => {
-                            res.status(500).json({err})
-                        })
+                    })
+                    .catch(err => {
+                        res.status(500).json({err})
+                    })
 
-                       
-                    })
-                    .catch(error => {
-                        res.status(500).json({error})
-                    })
-        
-                }else{
-                    res.status(200).json({
-                        message : "user does not exist"
-                    })
-                }
-            }
-            else{
-                res.status(403).json({
-                    message : "insufficient balance"
+                    
+                })
+                .catch(error => {
+                    res.status(500).json({error})
+                })
+    
+            }else{
+                res.status(200).json({
+                    message : "user does not exist"
                 })
             }
         })
